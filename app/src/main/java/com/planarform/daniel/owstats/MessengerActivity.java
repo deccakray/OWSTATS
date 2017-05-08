3afdc8ed4d3fc924e6ee2c3c80ef333de53f047a
Mark Mendoza <markolo25@gmail.com>
2017-05-08 05:30:01 -0700

    Connect is done, it's not working due to bad auth with firebase

--- app/src/main/java/com/planarform/daniel/owstats/MessengerActivity.java
+++ app/src/main/java/com/planarform/daniel/owstats/MessengerActivity.java
@@ -1,13 +1,37 @@
 package com.planarform.daniel.owstats;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MessengerActivity extends AppCompatActivity {
 
+    FloatingActionButton sendButton;
+    EditText message;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        sendButton = (FloatingActionButton)findViewById(R.id.send_button);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messenger);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                message = (EditText)findViewById(R.id.message);
+                FirebaseDatabase.getInstance()
+                        .getReference()
+                        .push()
+                        .setValue(new chatMessage(
+                                message.getText().toString(),"user"
+                        ));
+
+                message.setText("");
+
+            }
+        });
     }
 }
